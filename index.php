c151e8090197b1219f6bfa045b42cfbf18761805
obuhov.s <buhov.s@wezom.com.ua>
2019-06-27 19:37:32 +0300

    Planes changes it's will be a simple LOTO game

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php
 
 // Seeding
-mt_srand((double)microtime()*1000000);
+mt_srand((double)microtime() * 1000000);
 
 // generate numbers for the bingo card
 function generateCard()
@@ -12,18 +12,22 @@ function generateCard()
 
     $card = array();
 
+    $arrWithNumbers = [];
+    for ($number = 1; $number < 22; ++$number) {
+        $arrWithNumbers[$number] = $number;
+    }
 
-    for ($row = 1; $row < 7; ++$row)
-    {
+    for ($row = 1; $row < 6; ++$row) {
 
         $card[$row] = array();
 
-        $deck = array(0,1,2,3,4,5,6,7,8,9);
+        $deck = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
 
         // add 6 numbers to the row
         for ($rownumber = 0; $rownumber < 6; ++$rownumber) {
             // Random index
-            $index = mt_rand(0,count($deck) - 1);
+            $index = mt_rand(0, count($deck) - 1);
 
             // Random number from $deck
             $number = $deck[$index];
@@ -44,7 +48,7 @@ function generateCard()
     }
 
     // Last row
-    for ($col = 0; $col < 6; ++$col){
+    for ($col = 0; $col < 6; ++$col) {
         $card[7][$col] = 0;
     }
 
@@ -56,25 +60,25 @@ function generateCard()
 $card = generateCard();
 
 // Print card
-function printCard($card){ ?>
+function printCard($card)
+{ ?>
     <table border="1" cellspacing="0" cellpadding="5">
         <?php
         $row = 0;
-        foreach ($card as $index => $rij)
-        {
+        foreach ($card as $index => $rij) {
             $row++;
-            if ($row < 7)
-            {?>
+            if ($row < 7) {
+                ?>
                 <tr>
                 <?php
                 $column = 0;
-                foreach ($rij as $columnIndex => $number)
-                {
+                foreach ($rij as $columnIndex => $number) {
                     $column++;
-                    if ($column < 7)
-                    {       ?>
-                        <td<?php if (($rij[6]==6) || ($card[7][$column-1] ==6)) {  echo ' style="background-color:green"'; } ?>><?php echo $number ?></td>
-                    <?php    }
+                    if ($column < 7) { ?>
+                        <td<?php if (($rij[6] == 6) || ($card[7][$column - 1] == 6)) {
+                            echo ' style="background-color:green"';
+                        } ?>><?php echo $number ?></td>
+                    <?php }
                 }
             } ?>
             </tr>
@@ -86,18 +90,18 @@ function printCard($card){ ?>
 
 $getrokkenGetallen = array();
 
-$deck = range(10,69);
+$deck = range(10, 69);
 
 $bingo = false;
 
 // Keep drawing numbers till bingo is true
 // Keep drawing numbers till bingo is true
-while (!$bingo){
-    $index = mt_rand(0,count($deck) - 1);
+while (!$bingo) {
+    $index = mt_rand(0, count($deck) - 1);
 
     $number = $deck[$index];
 
-    if(!in_array($number, $getrokkenGetallen)){
+    if (!in_array($number, $getrokkenGetallen)) {
 
         unset($deck[$index]);
 
@@ -108,7 +112,7 @@ while (!$bingo){
         // Check if number is on the card
         for ($row = 0; $row < 7; $row++) {
             for ($rownumber = 0; $rownumber < 7; $rownumber++) {
-                if(isset($card[$row][$rownumber])){
+                if (isset($card[$row][$rownumber])) {
                     if ($card[$row][$rownumber] == $number) {
 
                         // set color?
@@ -116,7 +120,7 @@ while (!$bingo){
                         $card[$row][6] += 1; // Increment col
                         $card[7][$rownumber] += 1; // Increment row
                         // check if the 7th column or row contains 6 positive draws (5 for testing)
-                        if(($card[$row][6] == 6) || ($card[7][$rownumber] == 6)){
+                        if (($card[$row][6] == 6) || ($card[7][$rownumber] == 6)) {
                             $bingo = true;
                         }
                         break;
@@ -129,14 +133,13 @@ while (!$bingo){
 }
 
 
-if($bingo){
+if ($bingo) {
 
     printCard($card);
 
 
     echo '<p>Drawn numbers are:<br>';
-    foreach($getrokkenGetallen as $value)
-    {
+    foreach ($getrokkenGetallen as $value) {
         echo $value . ' ';
     }
     echo '</p>';
